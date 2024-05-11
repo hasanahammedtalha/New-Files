1256cecc06f4eed05c043ab4c880a09ae7fb7731
hasanahammedtalha <mdroknuzzamanzual@gmail.com>
2024-05-11 13:59:17 +0600

    New files bro

---
+++ NuFruit/system.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+    if( window.location == "https://talhabb.rf.gd/portfolio/demo-2/index.html") {
+        window.location = "https://talhabb.rf.gd/portfolio/demo-2/";
+    }
+
+}
